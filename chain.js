7f628fab2d1a1ffe0b228c30111da5ef54d7c878
adrianacedo <71908795+adrianacedo@users.noreply.github.com>
2020-11-25 21:28:35 -0500

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,20 @@
+class Chain{
+    constructor(bodyA,pointB){
+    var options =
+  { bodyA: bodyA,
+    pointB: pointB,
+    stiffness: 0.5,
+    length: 10 } 
+    this.chain = Constraint.create(options);
+    World.add(world, this.chain); 
+}
+fly(){ this.chain.bodyA =null; }
+
+    display(){
+        if(this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.pointB;
+        //line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+}
